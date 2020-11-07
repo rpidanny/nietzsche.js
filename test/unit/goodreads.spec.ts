a845bd6e63f64b0776a1ff3b5210270747d5fc78
Abhishek Maharjan <abhishekmaharjan1993@gmail.com>
2020-11-07 18:48:11 +0100

    test: improve test coverage (#7)
    * test: check path starts with /
    
    * test: add max pages test cases

--- test/unit/goodreads.spec.ts
+++ test/unit/goodreads.spec.ts
@@ -51,6 +51,13 @@ describe('Goodreads', () => {
       const quotes = gr.parseAndExtractQuotes(rawResponse.content_html)
       expect(quotes).toEqual(parsedQuotes)
     })
+
+    it('should filter out quotes with empty text', () => {
+      const htmlContent =
+        '<article>\n<div class=\'quoteContainer quoteContainer--noSeparator\'>\n<div class=\'quoteIconWrapper\'>\n<a aria-label="Orrin Woodward" href="/author/show/249881.Orrin_Woodward"><div class=\'userIcon\' style=\'background-image: url(https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1359585605i/249881._UY30_.jpg);\'></div>\n</a></div>\n<div class=\'quoteContentContainer\'>\n<div class=\'quote\'>\n<blockquote class=\'quoteBody\'>A limited state with free economic systems is the soil where the liberty tree blossoms.</blockquote>\n<div class=\'quoteDetails\'>\n<span class=\'quoteAuthor\'></span>\n<div class=\'quoteTags\'>\nTags:\n<a title="Economics quotes" href="/quotes/tag/economics">economics</a>,\n<a title="Freedom quotes" href="/quotes/tag/freedom">freedom</a>,\n<a title="Libertarianism quotes" href="/quotes/tag/libertarianism">libertarianism</a>,\n<a title="Liberty quotes" href="/quotes/tag/liberty">liberty</a>\n</div>\n<div class=\'quoteLikes\'>\n<ul class=\'quoteFooter\'>\n<li class=\'footerItem\'>\n<a id="like_action_quote_567718" rel="nofollow" data-like-count-id="like_count_quote_567718" class="jsLike" data-remote="true" data-method="post" href="/quotes/add/567718-a-limited-state-with-free-economic-systems-is-the-soil?format=json&amp;lc=3">Like</a>\n</li>\n<li class=\'footerItem ratingsCount\'>\n<a id="like_id_quote_567718" href="/quotes/567718-a-limited-state-with-free-economic-systems-is-the-soil"><span class=\'likesIndicator\'>\nlikes:\n</span>\n<span class=\'likesCount\'>3</span>\n</a>\n</li>\n</ul>\n\n</div>\n</div>\n</div>\n\n</div>\n</div>\n\n</article>\n'
+      const quotes = gr.parseAndExtractQuotes(htmlContent)
+      expect(quotes.length).toEqual(0)
+    })
   })
 
   describe('Quotes by Tag', () => {
@@ -70,6 +77,13 @@ describe('Goodreads', () => {
       const expectedResponse = new Array(5).fill(parsedQuotes[0])
       nock(baseUrl).get(/.*/).times(5).reply(200, rawResponse)
       const response = await gr.getAllQuotesByTag('economics')
+      expect(response).toEqual(expectedResponse)
+    })
+
+    it('should return parsed quotes from all pages until max pages', async () => {
+      const expectedResponse = new Array(2).fill(parsedQuotes[0])
+      nock(baseUrl).get(/.*/).times(2).reply(200, rawResponse)
+      const response = await gr.getAllQuotesByTag('economics', 10, 2)
       expect(response).toEqual(expectedResponse)
     })
 
@@ -151,6 +165,26 @@ describe('Goodreads', () => {
       const expectedResponse = new Array(5).fill(parsedQuotes[0])
       nock(baseUrl).get(/.*/).times(5).reply(200, rawResponse)
       const response = await gr.getAllQuotesByPath('quotes/tag/economics')
+      expect(response).toEqual(expectedResponse)
+    })
+
+    it('should fail when path starts with "/"', async () => {
+      expect.assertions(2)
+      try {
+        await gr.getAllQuotesByPath('/quotes/tag/economics')
+      } catch (err) {
+        expect(err).toBeInstanceOf(Error)
+        expect(err).toHaveProperty(
+          'message',
+          '`input` must not start with a slash when using `prefixUrl`',
+        )
+      }
+    })
+
+    it('should return parsed quotes from all pages until max pages', async () => {
+      const expectedResponse = new Array(2).fill(parsedQuotes[0])
+      nock(baseUrl).get(/.*/).times(2).reply(200, rawResponse)
+      const response = await gr.getAllQuotesByPath('quotes/tag/economics', 10, 2)
       expect(response).toEqual(expectedResponse)
     })
 
